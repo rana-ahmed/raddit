536087d3718d18ca6fa30fd77ad48a8514a41412
Rana Ahmed <minuddinrana@gmail.com>
2015-03-28 20:12:08 +0600

    Add user avatar through Gravatar

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -36,7 +36,7 @@ class LinksController < ApplicationController
 
     respond_to do |format|
       if @link.save
-        format.html { redirect_to @link, notice: 'Link was successfully created.' }
+        format.html { redirect_to @link, notice: 'Link was successfully created' }
         format.json { render :show, status: :created, location: @link }
       else
         format.html { render :new }
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+	def avatar_url(user, size, option)
+		email_adress = user.email.downcase
+		hash = Digest::MD5.hexdigest(email_adress)
+		img = "http://www.gravatar.com/avatar/#{hash}?s=#{size}"
+		profile = "http://www.gravatar.com/#{hash}"
+		if option == "img"
+			return img
+		elsif option == "profile"
+			return profile
+		else
+			return nil
+		end
+	end
 end
